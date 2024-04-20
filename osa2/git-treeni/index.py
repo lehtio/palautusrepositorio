6795755c12fa43cda35fe0e1a757b184aa547796
lehtio <laura.l.lehtio@gmail.com>
2024-04-20 22:45:46 +0300

    yhdistetään tässäkin brancheja

--- osa2/git-treeni/index.py
+++ osa2/git-treeni/index.py
@@ -1,6 +1,10 @@
 from logger import logger
+from summa import summa
+from erotus import erotus
 
 logger("aloitetaan")
 x = int(input("luku 1: "))
 y = int(input("luku 2: "))
+print(f"{summa(x,y)}")
+print(f"{erotus(x,y)}")
 logger("lopetetaan")
